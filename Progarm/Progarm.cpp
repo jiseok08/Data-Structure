24cb6f0bcddfa7c40f163619e6575484a0c3fc82
jiseok08 <heoji0912@gmail.com>
2025-07-16 21:44:47 +0900

    (2025.7.16) 이중 연결 리스트

--- Progarm/Progarm.cpp
+++ Progarm/Progarm.cpp
@@ -57,20 +57,134 @@ public:
             size--;
         }
     }
+
+    void push_back(T data)
+    {
+        Node* newNode = new Node;
+        
+        newNode->data = data;
+        newNode->next = nullptr;
+
+        if (head == nullptr)
+        {
+            head = newNode;
+        }
+        else
+        {
+            Node* currentNode = head;
+
+            while (currentNode->next != nullptr)
+            {
+                currentNode = currentNode->next;
+            }
+
+            currentNode->next = newNode;
+        }
+
+        size++;
+    }
+
+    void pop_back()
+    {
+
+        if (head == nullptr) cout << "linked list is empty" << endl;
+        else
+        {
+            Node* deleteNode = head;
+            Node* previousNode = nullptr;
+
+            if (size == 1)
+            {
+                head = deleteNode->next;
+
+                delete deleteNode;
+            }
+            else
+            {
+                while (deleteNode->next != nullptr)
+                {
+                    previousNode = deleteNode;
+
+                    deleteNode = deleteNode->next;
+                }
+
+                previousNode->next = nullptr;
+
+                delete deleteNode;
+            }
+            
+
+            size--;
+        }
+    }
+
+    void remove(T data)
+    {
+        if (head == nullptr) return;
+        else
+        {
+            Node* deleteNode = head;
+            Node* previousNode = nullptr;
+
+            while (deleteNode->next != nullptr)
+            {
+                if (deleteNode->data == data)
+                {
+                    if (deleteNode == head)
+                    {
+                        head = deleteNode->next;
+                    }
+                    else
+                    {
+                        previousNode->next = deleteNode->next;
+                    }
+
+                    delete deleteNode;
+
+                    deleteNode = previousNode;
+
+                    size--;
+                }
+
+                previousNode = deleteNode;
+
+                deleteNode = deleteNode->next;
+            }
+        }
+
+        return;
+    }
+
+    bool empty()
+    {
+        if (head == nullptr)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    ~List()
+    {
+        while (head != nullptr)
+        {
+            pop_front();
+        }
+    }
 };
 
 int main()
 {
     List<int> list;
 
-    list.push_front(50);
     list.push_front(20);
-    list.push_front(30);
 
-    list.pop_front();
-    list.pop_front();
-    list.pop_front();
-    list.pop_front();
+    list.remove(20);
+
+    cout << list.empty() << endl;
 
     return 0;
 }
