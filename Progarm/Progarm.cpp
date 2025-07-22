912ca24de8a7177a613159c8076d3b593aa7b7ad
jiseok08 <heoji0912@gmail.com>
2025-07-22 16:15:28 +0900

    (2025.7.22) 문자열

--- Progarm/Progarm.cpp
+++ Progarm/Progarm.cpp
@@ -2,8 +2,27 @@
 
 using namespace std;
 
+template <typename T>
+class Vector
+{
+private:
+	int count;
+	int capacity;
+
+	T* pointer;
+public:	
+	Vector()
+	{
+		count = 0;
+		capacity = 0;
+
+		pointer = nullptr;
+	}
+};
+
 int main()
 {
 
-    return 0;
+
+	return 0;
 }
