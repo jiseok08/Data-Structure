924d268abd1c485461491819cda6cf211efb4eb3
jiseok08 <heoji0912@gmail.com>
2025-08-07 18:37:38 +0900

    (2025.8.7) 이진 탐색 트리

--- Progarm/Progarm.cpp
+++ Progarm/Progarm.cpp
@@ -9,8 +9,14 @@ private:
 	struct Node
 	{
 		T data;
-		Node* left;
-		Node* right;
+
+		Node* left = nullptr;
+		Node* right = nullptr;
+
+		Node(T data)
+		{
+			this->data = data;
+		}
 	};
 
 	Node* root;
@@ -22,19 +28,11 @@ public:
 
 	void insert(T data)
 	{
-		Node* newNode = new Node;
-
-		newNode->data = data;
-		newNode->left = nullptr;
-		newNode->right = nullptr;
+		Node* newNode = new Node(data);
 
 		if (root == nullptr)
 		{
 			root = newNode;
-		}
-		else if (root->data == newNode->data)
-		{
-			return;
 		}
 		else
 		{
@@ -42,23 +40,86 @@ public:
 
 			while (currentNode != nullptr)
 			{
-				if (currentNode < newNode)
+				if (currentNode->data > data)
 				{
-					currentNode->right = newNode;
+					delete newNode;
+
+					return;
 				}
-				else if (currentNode > newNode)
+				else if (currentNode->data > data)
 				{
-					currentNode->left = newNode;
+					if (currentNode->left == nullptr)
+					{
+						currentNode->left = newNode;
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->left;
+					}
+				}
+				else
+				{
+					if (currentNode->right == nullptr)
+					{
+						currentNode->right = newNode;
+
+						break;
+					}
+					else
+					{
+						currentNode = currentNode->right;
+					}
 				}
 			}
 		}
+	}
+
+	void erase(T data)
+	{
+		Node* currentNode = root;
+
+		while (currentNode->data > data)
+		{
+			if (currentNode->data > data)
+			{
+				currentNode = currentNode->left;
+			}
+			else
+			{
+				currentNode = currentNode->right;
+			}
+		}
+	}
+
+	void release(Node* root)
+	{
+		if (root != nullptr)
+		{
+			release(root->left);
+
+			release(root->right);
+
+			delete root;
+		}
+	}
+
+	~Set()
+	{
+		release(root);
 	}
 };
 
 
 int main()
 {
+	Set<int> set;
 
+	set.insert(10);
+	set.insert(6);
+	set.insert(20);
+	set.insert(3);
+	set.insert(11);
 
 	return 0;
 }
